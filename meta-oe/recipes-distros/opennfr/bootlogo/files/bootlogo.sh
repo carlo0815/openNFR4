1642814e53b21f32d59f17b749fbb4eae397e4ec
toto <toto_golden@gmx.de>
2015-05-05 20:43:37 +0200

    Update bootlogo.sh

--- meta-oe/recipes-distros/opennfr/bootlogo/files/bootlogo.sh
+++ meta-oe/recipes-distros/opennfr/bootlogo/files/bootlogo.sh
@@ -2,17 +2,17 @@
 [ -f /sys/class/vtconsole/vtcon1/bind ] && echo 0 > /sys/class/vtconsole/vtcon1/bind
 # and set the correct videomode before showing the bootlogo
 [ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
-
-BOOTLOGO=/usr/share/bootlogo.mvi
-[ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
-/usr/bin/showiframe ${BOOTLOGO}
-[ -f /etc/init.d/bootlogo.py ] && /usr/bin/python /etc/init.d/bootlogo.py
-BOOTLOGO1=/usr/share/bootlogo1.mvi	
-	if [ -e /etc/.e2 ]; then
-		[ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
-		/usr/bin/showiframe ${BOOTLOGO}
-	elif [ -e /etc/.nhd2 ]; then
-		[ -f /etc/enigma2/bootlogo1.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo1.mvi
-		/usr/bin/showiframe ${BOOTLOGO1}
-	fi
+if [ "$(grep config.bootvideo.booting /etc/enigma2/settings)" ]; then
+    BOOTVIDEO=`grep config.bootvideo.booting /etc/enigma2/settings | cut -d"=" -f2`
+    gst-launch-1.0 playbin uri=file:///usr/share/enigma2/bootvideos/$BOOTVIDEO
+    BOOTLOGO=/usr/share/bootlogo.mvi
+    [ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
+    /usr/bin/showiframe ${BOOTLOGO}
+    [ -f /etc/init.d/bootlogo.py ] && /usr/bin/python /etc/init.d/bootlogo.py
+else
+    BOOTLOGO=/usr/share/bootlogo.mvi
+    [ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
+    /usr/bin/showiframe ${BOOTLOGO}
+    [ -f /etc/init.d/bootlogo.py ] && /usr/bin/python /etc/init.d/bootlogo.py
+fi
 
